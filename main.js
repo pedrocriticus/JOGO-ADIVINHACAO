658b812652c0c66758f44f236eb829e8540e0566
Pedro David <pedro.david.work@outlook.com>
2022-10-18 11:49:41 -0300

    Exercício 01 - level 05 - Curso: Explorer | RocketSeat

--- main.js
+++ main.js
@@ -1,67 +1,73 @@
+// variáveis botões html
 const screen1 = document.querySelector('.screen1');
 const screen2 = document.querySelector('.screen2');
 
-const randomNumber = Math.round(Math.random() * 10);
-let xAttempts = 1;
-console.log(randomNumber);
-
-
-//função CallBack
-function handleTryClick(event) {
-    event.preventDefault();// Não faça o padrão
-
-    const inputNumber = document.querySelector("#inputNumber")
-
-    
-    if (Number(inputNumber.value) == randomNumber) {
-       screen1.classList.add("hide")
-       screen2.classList.remove("hide")
-
-        document.
-        querySelector(".screen2 h2")
-        .innerText = `Acertou em ${xAttempts} tentativas!`;
-         
-}
-    inputNumber.value = "";
-    xAttempts++;
-   
-};
-
-//Eventos
+// variáveis Eventos
 const btnTry = document.querySelector("#btnTry");
-
 const btnReset = document.querySelector("#btnReset");
 
-//função callBack
+//Variável Calculo randõmico
+const randomNumber = Math.round(Math.random() * 10);
+
+//Variável de incrementação de tentativas
+let xAttempts = 1;
+console.log(`O número é ${randomNumber}.`);
+
+//Variável de input
+const inputNumber = document.querySelector("#inputNumber");
+
+
+
+//Eventos
 btnTry.addEventListener('click', handleTryClick);
+btnReset.addEventListener('click', handleResetClick);
 
-btnReset.addEventListener('click', function() {
+//funções CallBack
+function handleTryClick(event) {
+    event.preventDefault();// Não faça o padrão
+    if (Number(inputNumber.value) == randomNumber) {
+       toggleScreen();
+       screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas!`; 
+}   controllerInputNumber();
+    inputNumberValue();
+    inputNumber.value = "";
+    xAttempts++;
     
-    screen1.classList.remove("hide");
-    screen2.classList.add("hide");
+};
 
-    xAttempts = 1;
-    document.location.reload()
-})
+function handleResetClick() {
+    toggleScreen();
+        document.location.reload()
+};
 
+function toggleScreen() {
+    screen1.classList.toggle("hide") ;
+    screen2.classList.toggle("hide");
+}
 
+function inputNumberValue(number = '') {
+    if (inputNumber.value === number) {
+        xAttempts--
+        alert('Digite um número')
+    }
+}
 
+// function controllerInputNumber(number1) {
+//     let number0 = 0 ;
+//     let number10 = 10;
+//     number1 = 
 
-
-// let result = prompt("Adivinhe o número que estou pensando? Está entre 0 e 10");
-
-// const randomNumber = Math.round(Math.random() * 10);
-
-
-// let xAttempts = 1;
-
-// while(Number(result) != randomNumber) {
-//     result = prompt("Erro, tente novamente!");
-//     xAttempts++;
+//     if (Number(inputNumber.value) != number1)  {
+//         xAttempts--
+//         alert('Digite um número entre 0 e 10')
+//    }
+    
 // }
+/*
+Desafio qual é o evento de ENTER?
 
-// if(xAttempts === 1) {
-//     alert(`Parabéns! O número que eu pensei foi ${randomNumber} e você adivinhou o número em ${xAttempts} tentativa.`)
-// } else {
-//     alert(`Parabéns! O número que eu pensei foi ${randomNumber} e você adivinhou o número em ${xAttempts} tentativas.`)
-// }
+-fazer um limite entre 0 e 10
+
+-precisa ter número dentro do input para dar enter
+
+*/ 
